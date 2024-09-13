92334ba2a034b477423febd960253a47e23b6bea
Trickster <reone054@gmail.com>
2024-09-13 16:49:05 +0545

    created folder in mvc pattern and created main route page for user and admin, created connection for database with .env file

--- Daraz/backend/index.js
+++ Daraz/backend/index.js
@@ -1,12 +1,29 @@
 import express from "express";
+import 'dotenv/config'
+import connection from './model/config.js'
+import userRoute from './route/user.route.js'
+import adminRoute from './route/admin.route.js'
 
+
+
+//middleware and configuration 
 const server = express();
+server.use('/daraz', userRoute);
+server.use('/admin', adminRoute);
 
 
 server.get('/', (req,res)=>{
-    res.send('<h1>Server Started</h1>');
+    res.send('<h1>Project Started</h1>');
 })
 
-server.listen(8000, ()=>{
+server.listen(process.env.PORT, ()=>{
     console.log("Server is running");
+
+    try{
+        connection.authenticate()
+        console.log("Successfully connected to database")
+        connection.sync();
+    }catch{
+        console.log("Error during connection to database")
+    }
 })
